b7752adccb482417344e7e852dba27d0a73ebfb8
riedhampattel <riedhampattel.tops@gmail.com>
2025-02-24 15:56:02 +0530

    updated

---
+++ C Programming/Conditional statements/10.c
@@ -0,0 +1,1 @@
+//Write a c program to take a character from the user and check whether it is vowel or consonant.
---
+++ C Programming/Conditional statements/9.c
@@ -0,0 +1,1 @@
+//Write a c program to take a number from the user and check whether it is even or odd
