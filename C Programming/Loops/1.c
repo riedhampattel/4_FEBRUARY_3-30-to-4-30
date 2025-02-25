539e6831949591936a5a694eac20ef25706a58dc
riedhampattel <riedhampattel.tops@gmail.com>
2025-02-25 16:06:50 +0530

    updated

---
+++ C Programming/Loops/1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	printf("\nFor loop := ");
+	for(i=11;i<=5;i++)
+	{
+		printf("\nTops Technologies");//5
+	}
+	
+	printf("\nWhile loop := ");
+	i=11;
+	while(i<=5)
+	{
+		printf("\nTOPS Technologies");
+		i++;
+	}
+	
+	printf("\nDo while loop := ");
+	i=11;
+	do
+	{
+		printf("\nTOPS Technologies");
+		i++;
+	}while(i<=5);
+	return 0;
+}
---
+++ C Programming/Loops/2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	for(i=1;i<=10;i++)
+	{
+		printf("%d ",i);
+	}
+	printf("\n");
+	for(i=10;i>=1;i--)
+	{
+		printf("%d ",i);
+	}
+	return 0;
+}
