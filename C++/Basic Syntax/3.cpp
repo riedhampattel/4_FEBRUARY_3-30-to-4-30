d776495a4b800e7bb1b9b7e49fd72c060788ec28
riedhampattel <riedhampattel.tops@gmail.com>
2025-03-27 15:36:46 +0530

    updated

---
+++ C++/Basic Syntax/3.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter the value = ";
+    cin>>a;
+    cout<<"\nValue of a = "<<a;
+    return 0;
+}
---
+++ C++/Basic Syntax/4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 10;
+    float f = 2.56;
+    char ch = 'A';
+    string str = "Hello";
+    bool b1 = true;
+    bool b2 = false;
+    cout<<"\nValue of integer = "<<i;
+    cout<<"\nValue of float = "<<f;
+    cout<<"\nValue of char = "<<ch;
+    cout<<"\nValue of string = "<<str;
+    cout<<"\nValue of b1 = "<<b1;
+    cout<<"\nValue of b2 = "<<b2;
+    return 0;
+}
